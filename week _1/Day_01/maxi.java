18d5e1917fdcc470b26e8e1aa2130fe028b3c664
Archita Saxena <architaa145@gmail.com>
2026-06-01 23:21:50 +0530

    stock market

---
+++ week _1/Day_01/maxi.java
@@ -0,0 +1,19 @@
+public class maxi {
+    public static void main(String[] args) {
+
+        int[] prices = {7, 1, 5, 3, 6, 4};
+
+        int minPrice = prices[0];
+        int maxProfit = 0;
+
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] < minPrice) {
+                minPrice = prices[i];
+            } else {
+                maxProfit = Math.max(maxProfit, prices[i] - minPrice);
+            }
+        }
+
+        System.out.println(maxProfit);
+    }
+}
