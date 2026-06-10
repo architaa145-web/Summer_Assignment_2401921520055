de741bc19c316606683ad9ad6632373a1b2941fa
Archita Saxena <architaa145@gmail.com>
2026-06-10 21:09:03 +0530

    Added new code

---
+++ week_2/day_03/Main2.java
@@ -0,0 +1,20 @@
+
+public class Main2 {
+    public static void main(String[] args) {
+
+        String s = "abc";
+        String t = "ahbgdc";
+
+        int i = 0;
+        int j = 0;
+
+        while (i < s.length() && j < t.length()) {
+            if (s.charAt(i) == t.charAt(j)) {
+                i++;
+            }
+            j++;
+        }
+
+        System.out.println(i == s.length());
+    }
+}
