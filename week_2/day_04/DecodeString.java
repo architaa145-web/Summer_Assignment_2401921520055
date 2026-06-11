30e5416efef9dd39ead7ced98027a4fce0de295d
Archita Saxena <architaa145@gmail.com>
2026-06-11 21:41:08 +0530

    decode string

---
+++ week_2/day_04/DecodeString.java
@@ -0,0 +1,57 @@
+import java.util.*;
+
+public class DecodeString {
+
+    public static String decodeString(String s) {
+
+        Stack<Integer> countStack = new Stack<>();
+        Stack<StringBuilder> strStack = new Stack<>();
+
+        StringBuilder curr = new StringBuilder();
+        int num = 0;
+
+        for (char ch : s.toCharArray()) {
+
+            if (Character.isDigit(ch)) {
+                num = num * 10 + (ch - '0');
+            }
+
+            else if (ch == '[') {
+                countStack.push(num);
+                strStack.push(curr);
+
+                num = 0;
+                curr = new StringBuilder();
+            }
+
+            else if (ch == ']') {
+
+                int repeat = countStack.pop();
+                StringBuilder prev = strStack.pop();
+
+                for (int i = 0; i < repeat; i++) {
+                    prev.append(curr);
+                }
+
+                curr = prev;
+            }
+
+            else {
+                curr.append(ch);
+            }
+        }
+
+        return curr.toString();
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        String s = sc.nextLine();
+
+        System.out.println(decodeString(s));
+
+        sc.close();
+    }
+}
