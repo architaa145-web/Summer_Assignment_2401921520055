724a3e4d5a4d62a18cbf91ac0362f6a79a5ab27c
Archita Saxena <architaa145@gmail.com>
2026-06-16 03:12:22 +0530

    merge 2 list

---
+++ week_3/day_02/MergeTwoSortedLists.java
@@ -0,0 +1,73 @@
+public class MergeTwoSortedLists {
+
+    // Node definition
+    static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int val) {
+            this.val = val;
+        }
+    }
+
+    // Merge function
+    public static ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+
+        ListNode dummy = new ListNode(-1);
+        ListNode current = dummy;
+
+        while (list1 != null && list2 != null) {
+
+            if (list1.val <= list2.val) {
+                current.next = list1;
+                list1 = list1.next;
+            } else {
+                current.next = list2;
+                list2 = list2.next;
+            }
+
+            current = current.next;
+        }
+
+        // Attach remaining nodes
+        if (list1 != null) {
+            current.next = list1;
+        } else {
+            current.next = list2;
+        }
+
+        return dummy.next;
+    }
+
+    // Helper method to print a list
+    public static void printList(ListNode head) {
+        while (head != null) {
+            System.out.print(head.val);
+
+            if (head.next != null) {
+                System.out.print(" -> ");
+            }
+
+            head = head.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+
+        // List 1: 1 -> 2 -> 4
+        ListNode list1 = new ListNode(1);
+        list1.next = new ListNode(2);
+        list1.next.next = new ListNode(4);
+
+        // List 2: 1 -> 3 -> 4
+        ListNode list2 = new ListNode(1);
+        list2.next = new ListNode(3);
+        list2.next.next = new ListNode(4);
+
+        ListNode merged = mergeTwoLists(list1, list2);
+
+        System.out.println("Merged List:");
+        printList(merged);
+    }
+}
