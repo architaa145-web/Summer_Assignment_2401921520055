e3b8cea892219ff30f0a70ffd0bf6cc6dcf0670d
Archita Saxena <architaa145@gmail.com>
2026-06-16 19:24:10 +0530

    palindromelist

---
+++ week_3/day_02/PalindromeLinkedList.java
@@ -0,0 +1,55 @@
+public class PalindromeLinkedList {
+
+    static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int val) {
+            this.val = val;
+        }
+    }
+
+    public static boolean isPalindrome(ListNode head) {
+
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        ListNode prev = null;
+        ListNode curr = slow;
+
+        while (curr != null) {
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+
+        ListNode left = head;
+        ListNode right = prev;
+
+        while (right != null) {
+            if (left.val != right.val)
+                return false;
+
+            left = left.next;
+            right = right.next;
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(2);
+        head.next.next.next = new ListNode(1);
+
+        System.out.println(isPalindrome(head));
+    }
+}
