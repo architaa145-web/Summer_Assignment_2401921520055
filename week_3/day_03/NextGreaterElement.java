2cec8d9eaf0fc7cfc122b56133e09ffafc0953f7
Archita Saxena <architaa145@gmail.com>
2026-06-17 13:45:44 +0530

    next greater ele

---
+++ week_3/day_03/NextGreaterElement.java
@@ -0,0 +1,44 @@
+import java.util.*;
+
+public class NextGreaterElement {
+
+    public static int[] nextGreaterElement(int[] nums1, int[] nums2) {
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+        Stack<Integer> st = new Stack<>();
+
+        // Build next greater map using nums2
+        for (int i = nums2.length - 1; i >= 0; i--) {
+
+            while (!st.isEmpty() && st.peek() < nums2[i]) {
+                st.pop();
+            }
+
+            if (st.isEmpty()) {
+                map.put(nums2[i], -1);
+            } else {
+                map.put(nums2[i], st.peek());
+            }
+
+            st.push(nums2[i]);
+        }
+
+        int[] ans = new int[nums1.length];
+
+        for (int i = 0; i < nums1.length; i++) {
+            ans[i] = map.get(nums1[i]);
+        }
+
+        return ans;
+    }
+
+    public static void main(String[] args) {
+
+        int[] nums1 = {4, 1, 2};
+        int[] nums2 = {1, 3, 4, 2};
+
+        int[] result = nextGreaterElement(nums1, nums2);
+
+        System.out.println(Arrays.toString(result));
+    }
+}
