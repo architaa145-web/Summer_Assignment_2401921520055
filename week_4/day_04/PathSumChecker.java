ec4f4378f8310d2d897ff9e69fbf21b4e096a1e6
Archita Saxena <architaa145@gmail.com>
2026-07-09 03:59:53 +0530

    new code

---
+++ week_4/day_04/PathSumChecker.java
@@ -0,0 +1,46 @@
+
+class PathSumChecker {
+
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+    }
+
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+
+        if (root == null) {
+            return false;
+        }
+
+        if (root.left == null && root.right == null) {
+            return targetSum == root.val;
+        }
+
+        return hasPathSum(root.left, targetSum - root.val) ||
+               hasPathSum(root.right, targetSum - root.val);
+    }
+
+    public static void main(String[] args) {
+
+        PathSumChecker obj = new PathSumChecker();
+
+        TreeNode root = new TreeNode(5);
+        root.left = new TreeNode(4);
+        root.right = new TreeNode(8);
+
+        root.left.left = new TreeNode(11);
+        root.left.left.left = new TreeNode(7);
+        root.left.left.right = new TreeNode(2);
+
+        root.right.left = new TreeNode(13);
+        root.right.right = new TreeNode(4);
+        root.right.right.right = new TreeNode(1);
+
+        System.out.println(obj.hasPathSum(root, 22));
+    }
+}
