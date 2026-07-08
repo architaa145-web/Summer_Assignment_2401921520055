539d65748a1bfff270edc19bedea3ba01a9ccb6f
Archita Saxena <architaa145@gmail.com>
2026-07-09 04:02:02 +0530

    new code

---
+++ week_4/day_05/MaximumPathSumFinder.java
@@ -0,0 +1,45 @@
+
+class MaximumPathSumFinder {
+
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+    }
+
+    int ans = Integer.MIN_VALUE;
+
+    public int maxPathSum(TreeNode root) {
+        dfs(root);
+        return ans;
+    }
+
+    private int dfs(TreeNode root) {
+
+        if (root == null) {
+            return 0;
+        }
+
+        int left = Math.max(0, dfs(root.left));
+        int right = Math.max(0, dfs(root.right));
+
+        ans = Math.max(ans, left + right + root.val);
+
+        return Math.max(left, right) + root.val;
+    }
+
+    public static void main(String[] args) {
+
+        MaximumPathSumFinder obj = new MaximumPathSumFinder();
+
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+
+        System.out.println(obj.maxPathSum(root));
+    }
+}
